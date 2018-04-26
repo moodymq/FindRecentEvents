d870d9d71615236335e1679a4af99960189b446d
moodymq <mqmoody@gmail.com>
2018-04-26 19:32:08 -0400

    Update NInRange.R

--- NInRange.R
+++ NInRange.R
@@ -1,12 +1,13 @@
+library(lubridate)
+library(tidyverse)
+
 read.excel <- function(header=TRUE,...) {
   read.table("clipboard",sep="\t",header=header,...)
 }
 
 FindRecentEvents <- function(input, intervalType, intervalLength){
-  #theSum <- sum(x)
   offset <- do.call(intervalType, list(intervalLength))
   interv <- interval(input - offset, input - seconds(1)) #Remove a second so it doesn't count itself.
-  #apply(x, 1, function(y) y/sum(x))
   fx <- function(x,y) {
     count(filter(as.data.frame(y), y %within% x))
   }
